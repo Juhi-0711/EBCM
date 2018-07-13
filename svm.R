23ed962ea6f88c654942203e853b2f238780d3e1
Juhi Singh <Juhidpotterhead0711@gmail.com>
2018-07-13 16:21:18 +0530

    Update svm.R

--- svm.R
+++ svm.R
@@ -54,19 +54,20 @@ summary(data_MA)
 #Converting "Competency_code" to numeric form and normalizing it
 
 data[, 2] <- as.numeric(factor(data[, 2]))
-data[2:7] <- normalize(as.matrix(data[, 2:7]))
+summary(data)
+data[3:7]<-normalize(as.matrix(data[, 3:7]))
 
 data_SC[, 2] <- as.numeric(factor(data_SC[, 2]))
-data_SC[2:7] <- normalize(as.matrix(data_SC[, 2:7]))
+#data_SC[2:7] <- normalize(as.matrix(data_SC[, 2:7]))
 
 data_SS[, 2] <- as.numeric(factor(data_SS[, 2]))
-data_SS[2:7] <- normalize(as.matrix(data_SS[, 2:7]))
+#data_SS[2:7] <- normalize(as.matrix(data_SS[, 2:7]))
 
 data_ELA[, 2] <- as.numeric(factor(data_ELA[, 2]))
-data_ELA[2:7] <- normalize(as.matrix(data_ELA[, 2:7]))
+#data_ELA[2:7] <- normalize(as.matrix(data_ELA[, 2:7]))
 
 data_MA[, 2] <- as.numeric(factor(data_MA[, 2]))
-data_MA[2:7] <- normalize(as.matrix(data_MA[, 2:7]))
+#data_MA[2:7] <- normalize(as.matrix(data_MA[, 2:7]))
 
 
 #Capturing data information about completed and in-progress
@@ -85,6 +86,7 @@ closeAllConnections()
 #Splitting the data into test and train--->change here to change the dataset that you are working with.
 set.seed(1)
 n <- nrow(data)
+
 shuffled_df <- data[sample(n), ]
 train_indices <- 1:round(0.8 * n)
 train <- shuffled_df[train_indices, ]
@@ -101,7 +103,7 @@ dim(test)
 
 x_train<-subset(train,select= -status)
 y_train<-train$status 
-
+head(x_train)
 #svm_tune to find the parameters (cost, gamma)--->found on SC applied on everything--->done once
 #svm_tune <- tune(svm, train.x=x_train, train.y=y_train, kernel="radial", ranges=list(cost=10^(-2:2), gamma=2^(-2:2)))
 #print(svm_tune)
@@ -118,7 +120,7 @@ preds <- predict(model,x_train)
 
 #Plotting the data (expected vs predicted)
 mydf = cbind(train, preds)
-p=ggplot(train,aes(y=train$avg_time_spent, x= train$total_time_spent, color=train$status, shape=preds, size=train$num_resources/100) )+ geom_point(position=position_jitter(h=0.15,w=0.15))
+p=ggplot(train,aes(y=train$avg_time_spent, x= train$total_time_spent, color=train$status, shape=preds, size=train$num_resources/100) )
 p+geom_point(alpha=0.5)
 
 #Getting the accuracy
